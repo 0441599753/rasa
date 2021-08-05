b2bd67088b689d1cdbd72bc3a1ccfd70e2a05ca1
0441599753 <87757486+0441599753@users.noreply.github.com>
2021-08-05 11:23:33 +0430

    Create s.1 rasa.py

---
+++ s.1 rasa.py
@@ -0,0 +1,13 @@
+name = input("esm khod ra vared konid :")
+
+if name =='reza':
+    print("salam reza")
+
+elif name == 'rasa':
+    print("salam rasa")
+
+elif name == 'ali':
+    print("salam ali")
+
+else:
+    print("salam garibeh")
