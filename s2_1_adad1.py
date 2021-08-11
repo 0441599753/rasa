46e009ec3367f1a21281f5faa8a8a04f57fa7722
0441599753 <87757486+0441599753@users.noreply.github.com>
2021-08-11 17:32:18 +0430

    Create s2_1_adad1.py

---
+++ s2_1_adad1.py
@@ -0,0 +1,9 @@
+# salam man rasa ansari hastam va in proje adad ha zoj va fard ast
+# ke ba if va else neveshte shode ast
+number = int(input("adadi ra vared konid:"))
+number2 = (number % 2)
+if number2 == 0:
+    print("zoj")
+
+else:
+    print("fard")
