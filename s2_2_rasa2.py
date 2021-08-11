cc4b726fa11266596433fbf317708ab593654ccd
0441599753 <87757486+0441599753@users.noreply.github.com>
2021-08-11 17:48:34 +0430

    Create s2_2_rasa2.py

---
+++ s2_2_rasa2.py
@@ -0,0 +1,10 @@
+# salam man rasa ansari hastam va in proje bakhsh paziri ast
+# ke ba if va else neveshte shode ast
+number = int(input("adad aval ra vared konid:"))
+number2 = int(input("adad dovom ra vared konid :"))
+b = (number % number2)
+
+if b == 0:
+    print("bakhsh pazir ast")
+else:
+    print("bakhsh pazir nist")
