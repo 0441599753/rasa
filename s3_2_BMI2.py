a3e1338851a38a97b4b1f122960bb9126b50166b
0441599753 <87757486+0441599753@users.noreply.github.com>
2021-08-25 17:06:15 +0430

    Create s3_2_BMI2.py

---
+++ s3_2_BMI2.py
@@ -0,0 +1,29 @@
+# salam man rasa ansari hastam va in proje BMI ast
+# ke ba def va if va elif va return neveshte shode ast
+ghad = int(input("ghad khod ra konid:"))
+vazn = int(input("vazn khod ra konid:"))
+def BMI():
+    b =  vazn / (ghad * ghad) * 10000
+    return b
+c = BMI()
+r = round(c,2)
+print('''vazn shoma hast: {} va ghad shoma hast :{} va BMI shoma hast :{}'''.format(vazn , ghad ,r))
+if r < 16:
+    print("dochar kambod vazne shodehid")
+elif (r >= 16) and  (r <= 18):
+    print("kambod vazne")
+
+elif (r >= 18) and (r <= 25):
+    print("adi")
+
+elif (r >= 25) and (r <= 30):
+    print("ezafe vazn")
+
+elif (r >= 30) and (r <= 35):
+    print("ezafe vazne 1")
+
+elif (r >= 35) and (r <= 40):
+    print("ezafe vazne 2")
+
+elif (r > 40):
+    print("ezafe vazne 3")
