52bd2e8437ed5de4dc5a9fed4ff9a168dd60124f
0441599753 <87757486+0441599753@users.noreply.github.com>
2021-09-15 23:56:39 +0430

    Create s6_1_class.py

---
+++ s6_1_class.py
@@ -0,0 +1,9 @@
+# salam man rasa ansari hastam va in proje  class ast
+# ke ba def va if va index va list va else neveshte shode ast 
+def adam(person):
+    list = ['rasa' , 'reza' , 'parsa' , 'amir']
+    if person in list:
+        print(list.index(person))
+    else:
+        print(int(-1))
+adam('amir')            
