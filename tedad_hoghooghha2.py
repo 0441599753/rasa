f7f22adcfeae6e4cf65c2d0baed7b7305dd6e28b
0441599753 <87757486+0441599753@users.noreply.github.com>
2022-01-15 22:52:21 +0330

    Create tedad_hoghooghha2.py

---
+++ tedad_hoghooghha2.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+adam = ["rasa", "reza", "mohamad", "amir"]
+tedad_hoghoogh=[12, 6, 4, 9]
+
+
+plt.bar(adam, tedad_hoghoogh, color=["green" , "orange" , "red", "blue"])
+plt.title("tedad_hoghoogh_nafaraat")
+plt.ylabel("tedad_hoghooghha")
+plt.xlabel("namha")
+plt.savefig('nemodaar.png')
