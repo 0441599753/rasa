6421c3eef76dc2391fb0f9cbe6403f357a892dcc
0441599753 <87757486+0441599753@users.noreply.github.com>
2022-01-15 22:32:02 +0330

    Create tedad_hoghooghha.py

---
+++ tedad_hoghooghha.py
@@ -0,0 +1,8 @@
+adam = ["rasa", "reza", "mohamad", "amir"]
+tedad_hoghoogh=[12, 6, 4, 9]
+
+plt.bar(adam, tedad_hoghoogh, color=["green" , "orange" , "red", "blue"])
+plt.title("tedad_hoghoogh_nafaraat")
+plt.ylabel("tedad_hoghooghha")
+plt.xlabel("namha")
+plt.show()
